8b8f65f0efeea45571d241d7f45fd865b9ca74bb
esnokum-dacom <esnokaskas@gmail.com>
2025-11-09 20:36:06 -0600

    Add the Media Log, but this doesn't have function yet, tomorrow im going to add this

--- Cont/Explorer/Scripts/Links.js
+++ Cont/Explorer/Scripts/Links.js
@@ -3,3 +3,4 @@ import initRendersButton from "../../ScriptsAll/Window.js";
 const Renders = document.getElementById("CreateCW");
 
 initRendersButton("CreateCW", "Renders", "../../../Props/Icons/MIE.png", "Cont/Renders/index.html", 600, 500);
+initRendersButton("CreateBl", "Blog", "../../../Props/Icons/MIE.png", "Cont/Blog/index.html", 600, 500);

bdac7e866f32fd2f85c9381b6036c873d692572d
esnokum-dacom <esnokaskas@gmail.com>
2025-11-11 19:05:46 -0600

    Add more modules

--- Cont/Explorer/Scripts/Links.js
+++ Cont/Explorer/Scripts/Links.js
@@ -4,3 +4,5 @@ const Renders = document.getElementById("CreateCW");
 
 initRendersButton("CreateCW", "Renders", "../../../Props/Icons/MIE.png", "Cont/Renders/index.html", 600, 500);
 initRendersButton("CreateBl", "Blog", "../../../Props/Icons/MIE.png", "Cont/Blog/index.html", 600, 500);
+initRendersButton("CreateTS", "Tastes", "../../../Props/Icons/MIE.png", "Cont/MyTastes/index.html", 600, 400);
+
---
+++ Cont/MyTastes/Buttons/Nav.js
@@ -0,0 +1,6 @@
+import initRendersButton from "../../ScriptsAll/Window.js";
+
+initRendersButton("CreateCW", "Music & Albums", "../../../Props/Icons/MIE.png", "Cont/MyTastes/Topics/Music/index.html", 600, 500);
+initRendersButton("CreateBl", "Video Games", "../../../Props/Icons/MIE.png", "Cont/MyTastes/Topics/VGames/index.html", 600, 500);
+initRendersButton("CreateTS", "Series & Films", "../../../Props/Icons/MIE.png", "Cont/MyTastes/Topics/Series&Films/index.html", 600, 400);
+
